c79605e7cc48b2fb1270e710d53da50a6f16232a
Chitiki Saritha <148208034+ChitikiSaritha@users.noreply.github.com>
2023-10-17 17:31:37 +0530

    Add files via upload

---
+++ ExplicitTypeCastingDemo.java
@@ -0,0 +1,13 @@
+
+public class ExplicitTypeCastingDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double d=45.32;
+		int i=(int)d;
+		System.out.println("d="+d);
+		System.out.println("i="+i);
+
+	}
+
+}
